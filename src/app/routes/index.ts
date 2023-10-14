43fa0ea6bee8c0befd758ff3e306988ded26121f
masud90895 <masudhossainmbs129@gmail.com>
2023-10-14 16:29:50 +0600

    category added

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { AuthRoutes } from '../modules/auth/auth.routes';
 import { UserRoutes } from '../modules/users/user.routes';
+import { SpecializationRoutes } from '../modules/Specialization/specialization.routes';
+import { CategoryRoutes } from '../modules/category/category.routes';
 
 const router = express.Router();
 
@@ -12,6 +14,14 @@ const moduleRoutes = [
   {
     path: '/users',
     route: UserRoutes,
+  },
+  {
+    path: '/specialization',
+    route: SpecializationRoutes,
+  },
+  {
+    path: '/categories',
+    route: CategoryRoutes,
   },
 ];
 
